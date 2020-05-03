c3eeabafe6f77b828f4e6472f9601c421a498be8
oguzhan18m <62189492+oguzhan18m@users.noreply.github.com>
2020-05-04 02:08:32 +0300

    tourdata changed

--- src/components/tourData.js
+++ src/components/tourData.js
@@ -5,7 +5,7 @@ export const tourData = [
     img: "./img/newyork.jpeg",
     name: "new york bridge tour",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "New York 30M nüfusa sahip ABD'nin en kalabalık kentidir ve Kuzey Doğuda yer alır.Özgürlük Anıtı ise görülmeye değer en önemli turistik yapıdır."
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const tourData = [
     img: "./img/paris.jpeg",
     name: "paris lights tour",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "Paris Avrupa'nın moda merkezlerinden biri olarak tanımlanır. Şehirde Araplar ve Çinlilerde yoğun olarak yaşamaktadır. Eiffel Tower, Sacre Coeur, Notre Dame görülmeye değer yapılardır."
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const tourData = [
     img: "./img/london.jpeg",
     name: "london royal palace tour",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "Londra İngiltere'nin başkentidir.Londra'daki saat kulesi ve yanındaki köprü muğazzamdır.Oxford gibi dünya klasındaki üniversitelere ev sahipliği yapar."
   },
   {
     id: 4,
@@ -29,6 +29,30 @@ export const tourData = [
     img: "./img/tokyo.jpeg",
     name: "tokyo sushi tour",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "Tokyoyu sikiym!"
+  }
+  {
+    id: 5,
+    city: "jackson",
+    img: "./img/jackson.jpg",
+    name: "jackson hole",
+    info:
+      "Jackson Hole Wyoming eyaletindedir ve Teton Dağları dibindeki bir Town'dur.Yellowstone National Park'a 1 saat uzaklıkta ve Grand Teton National Park'a 15 dk uzaklıktadır.On numara yerdir. "
+  }
+  {
+    id: 6,
+    city: "lasvegas",
+    img: "./img/lasvegas.jpeg",
+    name: "vegas baby",
+    info:
+      "What happens in Vegas, stays in Vegas!"
+  }
+  {
+    id: 7,
+    city: "sanfrancisco",
+    img: "./img/sanfrancisco.jpeg",
+    name: "the city",
+    info:
+      "San Francisco büyüleyici bir şehirdir. Golden State Warriors bu şehrin takımıdır. Şehirde Alcatraz Hapishanesi ise çok önemli bir turistik noktadır. Lombard Streette arabanız ile menderesler çizebilirsiniz."
   }
 ];
